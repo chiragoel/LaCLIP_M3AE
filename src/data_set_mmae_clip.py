2307bc83c18989e2eac1f0a99d73b1b111cca64a
Chirag Goel <chiragoel3@gmail.com>
2024-04-16 01:23:58 +0000

    Changed dataloader

--- src/data_set_mmae_clip.py
+++ src/data_set_mmae_clip.py
@@ -8,29 +8,122 @@ import torchvision.transforms as transforms
 logger = logging.getLogger(__name__)
 WORKING_PATH="/home/chirag/projects/def-sponsor00/sarcasticcodecrew/MMSD2.0/data"
 
+# class MyDataset(Dataset):
+#     def __init__(self, mode, text_name, limit=None, is_augs=True):
+#         self.mode = mode
+#         self.image_dim = 224
+#         self.text_name = text_name
+#         self.data = self.load_data(mode, limit)
+#         self.image_ids=list(self.data.keys())
+#         for id in self.data.keys():
+#             self.data[id]["image_path"] = os.path.join(WORKING_PATH,"dataset_image",str(id)+".jpg")
+        
+#         if self.mode=='train' and is_augs==True:
+#             self.transform = transforms.Compose([
+#             transforms.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),
+#             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+#             transforms.RandomHorizontalFlip()
+#             ])
+#         elif self.mode=='train':
+#             self.transform = transforms.Compose([
+#             transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.8, 1.0))
+#             ])
+#         else:
+#             self.transform = transforms.Compose([
+#             transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.8, 1.0))
+#             ])
+    
+#     def load_data(self, mode, limit):
+#         cnt = 0
+#         data_set=dict()
+#         if mode in ["train"]:
+#             f1= open(os.path.join(WORKING_PATH, self.text_name ,mode+".json"),'r',encoding='utf-8')
+#             datas = json.load(f1)
+#             for data in datas:
+#                 if limit != None and cnt >= limit:
+#                     break
+
+#                 image = data['image_id']
+#                 sentence = data['text']
+#                 label = data['label']
+ 
+#                 if os.path.isfile(os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")):
+#                     data_set[int(image)]={"text":sentence, 'label': label}
+#                     cnt += 1
+                    
+        
+#         if mode in ["test","valid"]:
+#             f1= open(os.path.join(WORKING_PATH, self.text_name ,mode+".json"),'r',encoding='utf-8')
+#             datas = json.load(f1)
+#             for data in datas:
+#                 image = data['image_id']
+#                 sentence = data['text']
+#                 label = data['label']
+
+#                 if os.path.isfile(os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")):
+#                     data_set[int(image)]={"text":sentence, 'label': label}
+#                     cnt += 1
+#         return data_set
+
+
+#     def image_loader(self,id):
+#         return Image.open(self.data[id]["image_path"])
+#     def text_loader(self,id):
+#         return self.data[id]["text"]
+
+
+#     def __getitem__(self, index):
+#         id=self.image_ids[index]
+#         text = self.text_loader(id)
+#         image_feature = self.transform(self.image_loader(id))
+#         label = self.data[id]["label"]
+#         return text,image_feature, label, id
+
+#     def __len__(self):
+#         return len(self.image_ids)
+#     @staticmethod
+#     def collate_func(batch_data):
+#         batch_size = len(batch_data)
+ 
+#         if batch_size == 0:
+#             return {}
+
+#         text_list = []
+#         image_list = []
+#         label_list = []
+#         id_list = []
+#         for instance in batch_data:
+#             text_list.append(instance[0])
+#             image_list.append(instance[1])
+#             label_list.append(instance[2])
+#             id_list.append(instance[3])
+#         return text_list, image_list, label_list, id_list
+
+
 class MyDataset(Dataset):
-    def __init__(self, mode, text_name, limit=None, is_augs=True):
+    def __init__(self, mode, text_name, limit=None, is_augs=False):
         self.mode = mode
         self.image_dim = 224
         self.text_name = text_name
         self.data = self.load_data(mode, limit)
-        self.image_ids=list(self.data.keys())
-        for id in self.data.keys():
-            self.data[id]["image_path"] = os.path.join(WORKING_PATH,"dataset_image",str(id)+".jpg")
+        # self.image_ids=list(self.data.keys())
+        # for id in self.data.keys():
+        #     self.data[id]["image_path"] = os.path.join(WORKING_PATH,"dataset_image",str(id)+".jpg")
         
+        is_augs = False
         if self.mode=='train' and is_augs==True:
             self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),
+            transforms.RandomResizedCrop(size=(224, 224), scale=(0.5, 1.0)),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             transforms.RandomHorizontalFlip()
             ])
         elif self.mode=='train':
             self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.8, 1.0))
+            transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.5, 1.0))
             ])
         else:
             self.transform = transforms.Compose([
-            transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.8, 1.0))
+            transforms.RandomResizedCrop(size=(self.image_dim, self.image_dim), scale=(0.5, 1.0))
             ])
     
     def load_data(self, mode, limit):
@@ -38,6 +131,7 @@ class MyDataset(Dataset):
         data_set=dict()
         if mode in ["train"]:
             f1= open(os.path.join(WORKING_PATH, self.text_name ,mode+".json"),'r',encoding='utf-8')
+            f2= open(os.path.join('/home/chirag/projects/def-sponsor00/chirag/CLIP-MA-MMSD/data/augmented_data (1).json'),'r',encoding='utf-8')
             datas = json.load(f1)
             for data in datas:
                 if limit != None and cnt >= limit:
@@ -48,7 +142,19 @@ class MyDataset(Dataset):
                 label = data['label']
  
                 if os.path.isfile(os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")):
-                    data_set[int(image)]={"text":sentence, 'label': label}
+                    data_set[int(cnt)]={"text":sentence, 'label': label, 'image_path': os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")}
+                    cnt += 1
+            datas_augs = json.load(f2)
+            for data in datas_augs:
+                if limit != None and cnt >= limit:
+                    break
+
+                image = data['image_id']
+                sentence = data['text']
+                label = data['label']
+ 
+                if os.path.isfile(os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")):
+                    data_set[int(cnt)]={"text":sentence, 'label': label, 'image_path': os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")}
                     cnt += 1
                     
         
@@ -61,9 +167,11 @@ class MyDataset(Dataset):
                 label = data['label']
 
                 if os.path.isfile(os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")):
-                    data_set[int(image)]={"text":sentence, 'label': label}
+                    data_set[int(cnt)]={"text":sentence, 'label': label, 'image_path': os.path.join(WORKING_PATH,"dataset_image",str(image)+".jpg")}
                     cnt += 1
         return data_set
+
+
 
 
     def image_loader(self,id):
@@ -73,29 +181,28 @@ class MyDataset(Dataset):
 
 
     def __getitem__(self, index):
-        id=self.image_ids[index]
+        id=index
         text = self.text_loader(id)
         image_feature = self.transform(self.image_loader(id))
         label = self.data[id]["label"]
         return text,image_feature, label, id
 
     def __len__(self):
-        return len(self.image_ids)
-    @staticmethod
-    def collate_func(batch_data):
-        batch_size = len(batch_data)
+        return len(self.data.keys())
+#     @staticmethod
+#     def collate_func(batch_data):
+#         batch_size = len(batch_data)
  
-        if batch_size == 0:
-            return {}
+#         if batch_size == 0:
+#             return {}
 
-        text_list = []
-        image_list = []
-        label_list = []
-        id_list = []
-        for instance in batch_data:
-            text_list.append(instance[0])
-            image_list.append(instance[1])
-            label_list.append(instance[2])
-            id_list.append(instance[3])
-        return text_list, image_list, label_list, id_list
-
+#         text_list = []
+#         image_list = []
+#         label_list = []
+#         id_list = []
+#         for instance in batch_data:
+#             text_list.append(instance[0])
+#             image_list.append(instance[1])
+#             label_list.append(instance[2])
+#             id_list.append(instance[3])
+#         return text_list, image_list, label_list, id_list

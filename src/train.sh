5a108792db57d77524998f0f7013197c0ccc31b1
chiragoel <39792413+chiragoel@users.noreply.github.com>
2024-04-29 20:43:27 -0400

    Update train.sh

--- src/train.sh
+++ src/train.sh
@@ -32,8 +32,8 @@ echo "Exp 9: MV_CLIP no MMAE"
 python3 main.py --model=MV_CLIP --text_name=text_json_final --weight_decay=0.05 --train_batch_size=32 --dev_batch_size=8 --learning_rate=5e-4 --clip_learning_rate=1e-6 --num_train_epochs=1 --layers=3 --max_grad_norm=5 --dropout_rate=0.1 --warmup_proportion=0.2 --device=0 --output_dir=../models/final_clip_nommae > ../models/logs/final_clip_nommae.txt 
 
 echo "Exp 10: MV_CLIP_original no MMAE"
-!python3 main.py --model=MV_CLIP_original --text_name=text_json_final --weight_decay=0.05 --train_batch_size=8 --dev_batch_size=8 --learning_rate=5e-4 --clip_learning_rate=1e-6 --num_train_epochs=10 --layers=3 --max_grad_norm=5 --dropout_rate=0.1 --warmup_proportion=0.2 --device=0 --output_dir=../models/final_clip_original_nommae > ../models/logs/final_clip_original_nommae.txt 
+python3 main.py --model=MV_CLIP_original --text_name=text_json_final --weight_decay=0.05 --train_batch_size=8 --dev_batch_size=8 --learning_rate=5e-4 --clip_learning_rate=1e-6 --num_train_epochs=10 --layers=3 --max_grad_norm=5 --dropout_rate=0.1 --warmup_proportion=0.2 --device=0 --output_dir=../models/final_clip_original_nommae > ../models/logs/final_clip_original_nommae.txt 
 
 echo "Exp 11: MV_CLIP_original MMAE"
-!python3 main.py --model=MV_CLIP_original --text_name=text_json_final --replicate_mmae=True --weight_decay=0.05 --train_batch_size=8 --dev_batch_size=8 --learning_rate=5e-4 --clip_learning_rate=1e-6 --num_train_epochs=10 --layers=3 --max_grad_norm=5 --dropout_rate=0.1 --warmup_proportion=0.2 --device=0 --output_dir=../models/final_clip_original_mmae > ../models/logs/final_clip_original_mmae.txt 
+python3 main.py --model=MV_CLIP_original --text_name=text_json_final --replicate_mmae=True --weight_decay=0.05 --train_batch_size=8 --dev_batch_size=8 --learning_rate=5e-4 --clip_learning_rate=1e-6 --num_train_epochs=10 --layers=3 --max_grad_norm=5 --dropout_rate=0.1 --warmup_proportion=0.2 --device=0 --output_dir=../models/final_clip_original_mmae > ../models/logs/final_clip_original_mmae.txt 
 
